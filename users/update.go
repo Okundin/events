2a00d0e734c49a57e25d3c19a163933a5a2e9729
okundin <okundin@yandex.ru>
2024-06-12 22:32:02 +0300

    moved model to sep file

--- users/update.go
+++ users/update.go
@@ -2,7 +2,6 @@ package users
 
 import (
 	"events-app/db"
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -10,17 +9,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type UserUpdate struct {
-	ID        int64
-	FirstName string `json:"first_name" binding:"required"`
-	LastName  string `json:"last_name" binding:"required"`
-}
-
 func Update(ctx *gin.Context) {
 	// parsing user PUT HTTP request
 	userID, err := strconv.ParseInt(ctx.Param("id"), 10, 64) // 10: decimal, 64: int64 type
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	// verify if user exists in the users table
@@ -31,9 +24,8 @@ func Update(ctx *gin.Context) {
 	}
 	// parsing body of the user HTTP request
 	err = ctx.ShouldBindJSON(&user)
-	fmt.Println(user)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
@@ -41,17 +33,17 @@ func Update(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"Success": "First and last name updated!"})
 }
 
-func GetUserByID(userID int64) (*UserUpdate, error) {
+func GetUserByID(userID int64) (UserUpdate, error) {
 	// verify if user requesting name update exists in the users DB table
 	queryUser := `SELECT id FROM users WHERE id = $1;`
 	row := db.DB.QueryRow(queryUser, userID)
 	var u UserUpdate
 	err := row.Scan(&u.ID)
 	if err != nil {
-		return &UserUpdate{}, err
+		return UserUpdate{}, err
 	}
 
-	return &u, nil
+	return u, nil
 }
 
 func (u *UserUpdate) updateName() {
