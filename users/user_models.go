f3f8981d149dec725dc0a3305ffa85ee3a208101
okundin <okundin@yandex.ru>
2024-06-12 22:30:32 +0300

    moved models to sep file

---
+++ users/user_models.go
@@ -0,0 +1,17 @@
+package users
+
+import "time"
+
+type User struct {
+	ID        int64
+	Login     string `json:"login" binding:"required,min=3"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required,min=4"`
+	CreatedAt time.Time
+}
+
+type UserUpdate struct {
+	ID        int64
+	FirstName string `json:"first_name" binding:"required,min=3"`
+	LastName  string `json:"last_name" binding:"required,min=2"`
+}
