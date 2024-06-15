0def1e37c75cf31735980ea0f3330ed5632f6582
okundin <okundin@yandex.ru>
2024-06-15 22:05:22 +0300

    added middleware authentication

---
+++ middleware/auth.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"events-app/users"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Authenticate verifies the token provided by the user in the HTTP request
+func Authenticate(ctx *gin.Context) {
+	// here we parse the token provided in the request
+	token := ctx.Request.Header.Get("Authorization")
+
+	//case token is empty
+	if token == "" {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Not authorized!"})
+		return
+	}
+	// case token is not valid; we also get a UserID value from the token to identify the user sending the HTTP request
+	userID, err := users.VerifyToken(token)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Not authorized!"})
+		return
+	}
+
+	ctx.Set("userID", userID) //allows to use userID value everywhere where *gin.Context is present
+	ctx.Next()
+
+}
